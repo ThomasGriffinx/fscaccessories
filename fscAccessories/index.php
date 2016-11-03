c8bd1b0cf4aefd13367413a89e6ba9b5b5503aa4
thomasgriffinx <xytomg@gmail.com>
2016-11-03 12:51:46 +0000

    try

--- fscAccessories/index.php
+++ fscAccessories/index.php
@@ -128,9 +128,6 @@
 	<script type="text/javascript" src="js/jquery.animate-colors-min.js"></script>
 	<script type="text/javascript" src="js/jquery.animate-shadow-min.js"></script>    
 	<script type="text/javascript" src="js/main.js"></script>
-	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  	<script src="../js/materialize.js"></script>
-  	<script src="../js/init.js"></script>
 	<script>
 
 /* PRELOADER */
